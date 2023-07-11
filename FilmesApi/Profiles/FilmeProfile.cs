20315bbf4a9f72461858d03fad612fca83f67758
Yasmin <ys.darc@gmail.com>
2023-07-11 18:09:16 -0300

    inserindo verbos http put, patch e delete

--- FilmesApi/Profiles/FilmeProfile.cs
+++ FilmesApi/Profiles/FilmeProfile.cs
@@ -8,5 +8,8 @@ public class FilmeProfile : Profile
     public FilmeProfile()
     {
         CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
     }
 }
